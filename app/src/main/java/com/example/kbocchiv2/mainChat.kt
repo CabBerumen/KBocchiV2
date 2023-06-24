f70122d7c62588f32fc43a233b514d8bd6b17a7f
pauloro371 <orozcopaulo6@gmail.com>
2023-06-23 21:20:47 -0600

    Cambios en el activity mainChat(cambio de diseño e implementacion del drawer )

--- app/src/main/java/com/example/kbocchiv2/mainChat.kt
+++ app/src/main/java/com/example/kbocchiv2/mainChat.kt
@@ -1,21 +1,39 @@
 package com.example.kbocchiv2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.preference.PreferenceManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import io.socket.client.IO
 import io.socket.client.Socket
 import io.socket.emitter.Emitter
 import java.net.URISyntaxException
 
-class mainChat : AppCompatActivity() {
+class mainChat : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
 
     private lateinit var socket: Socket
     private lateinit var editTextMessage: EditText
     private lateinit var buttonSend: Button
     private lateinit var textViewChat: TextView
+    var mAuth: FirebaseAuth? = null
+    var mGoogleSignInClient: GoogleSignInClient? = null
+    var navigationView: NavigationView? = null
+    var toolbar: Toolbar? = null
+    var drawerLayout: DrawerLayout? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,11 +60,41 @@ class mainChat : AppCompatActivity() {
                 displayMessage(message)
             }
         })
+
+
+        setSupportActionBar(toolbar)
+        toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+
+        drawerLayout?.closeDrawer(GravityCompat.START)
+        mAuth = FirebaseAuth.getInstance()
+        navigationView?.setNavigationItemSelectedListener(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_close,
+            R.string.navigation_drawer_close
+        )
+
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+
+
     }
 
     private fun setupSocket() {
         try {
-            socket = IO.socket("http://<IP_DEL_SERVIDOR>:3000")
+            socket = IO.socket("https://kbocchi.onrender.com/")
         } catch (e: URISyntaxException) {
             e.printStackTrace()
         }
@@ -68,4 +116,55 @@ class mainChat : AppCompatActivity() {
         super.onDestroy()
         socket.disconnect()
     }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_item0 -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.nav_logout -> {
+                mAuth!!.signOut()
+                mGoogleSignInClient!!.signOut()
+                val intent = Intent(this, LogIn::class.java)
+                startActivity(intent)
+                finish()
+                //Cerrar Sesión
+                val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+                val editor = sharedPreferences.edit()
+                editor.remove("token")
+                editor.apply()
+                val intent2 = Intent(this, LogIn::class.java)
+                startActivity(intent2)
+                finish()
+
+            }
+            R.id.nav_pacientes -> {
+                val intent = Intent(this, Pacientes::class.java)
+                startActivity(intent)
+            }
+
+            R.id.nav_item2 -> {
+                val intent = Intent(this, mainChat::class.java)
+                startActivity(intent)
+            }
+        }
+        drawerLayout!!.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed() {
+        if (drawerLayout!!.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout!!.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
+
+
+
+
